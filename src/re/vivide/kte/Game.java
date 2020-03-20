c553d8d9d21d824f0df36f283207c5f8cc3f5704
Vidalee <thomas.sirvent@free.fr>
2020-03-20 09:41:27 +0100

    scoreboard things

--- src/re/vivide/kte/Game.java
+++ src/re/vivide/kte/Game.java
@@ -15,25 +15,34 @@ public class Game {
     Scoreboard kte_scoreboard;
     private ArrayList<Player> players = new ArrayList<>();
     boolean active;
+    private String sc_playersCount = "Joueurs : " + ChatColor.AQUA + "0";
+    private Objective obj;
 
     public Game(KTE instance){
         active = true;
         kte = instance;
         kte_scoreboard = kte.getServer().getScoreboardManager().getNewScoreboard();
-        Objective obj = kte_scoreboard.registerNewObjective("KTE_board", "dummy");
+        obj = kte_scoreboard.registerNewObjective("KTE_board", "dummy");
         obj.setDisplaySlot(DisplaySlot.SIDEBAR);
         obj.setDisplayName(ChatColor.DARK_PURPLE + "Kill The EALD 2");
         obj.getScore(" ").setScore(10);
-        Timer timer = new Timer(obj, instance);
     }
 
     public void addPlayer(Player p){
         players.add(p);
         p.setScoreboard(kte_scoreboard);
+        updatePlayersCount();
     }
 
     public void removePlayer(Player p ){
         players.remove(p);
         p.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
+        updatePlayersCount();
+    }
+
+    private void updatePlayersCount(){
+        kte_scoreboard.resetScores(sc_playersCount);
+        sc_playersCount = "Joueurs : " + ChatColor.AQUA + players.size();
+        obj.getScore(sc_playersCount).setScore(9);
     }
 }
--- src/re/vivide/kte/MainListener.java
+++ src/re/vivide/kte/MainListener.java
@@ -4,11 +4,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class MainListener implements Listener{
 
     @EventHandler
     public void onPlayerChangedWorld(PlayerChangedWorldEvent event){
+        Player p = event.getPlayer();
+        if(p.getWorld().getName().contains("kte")){
+            KTE.game.addPlayer(p);
+        }else{
+            KTE.game.removePlayer(p);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
         Player p = event.getPlayer();
         if(p.getWorld().getName().contains("kte")){
             KTE.game.addPlayer(p);
