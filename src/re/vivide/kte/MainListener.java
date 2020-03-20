31c1d979582754fd8475d66c37372346de8af526
Vidalee <thomas.sirvent@free.fr>
2020-03-20 10:51:51 +0100

    riding carpets

--- src/re/vivide/kte/MainListener.java
+++ src/re/vivide/kte/MainListener.java
@@ -1,10 +1,18 @@
 package re.vivide.kte;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class MainListener implements Listener{
 
@@ -25,6 +33,25 @@ public class MainListener implements Listener{
             KTE.game.addPlayer(p);
         }else{
             KTE.game.removePlayer(p);
+        }
+    }
+
+    @EventHandler
+    public void onInteractEvent(PlayerInteractEvent event){
+        Player p = event.getPlayer();
+        Block b = event.getClickedBlock();
+        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType() == Material.PINK_CARPET){
+            Arrow arrow = p.getWorld().spawn(b.getLocation().add(0.5D, 0.2D, 0.5D), Arrow.class);
+            arrow.addPassenger(p);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDismountEvent(EntityDismountEvent event){
+        Entity entity = event.getEntity();
+        Entity dismounted = event.getDismounted();
+        if(entity.getType().equals(EntityType.PLAYER) && dismounted.getType().equals(EntityType.ARROW)) {
+            dismounted.remove();
         }
     }
 }
